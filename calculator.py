6a239a5ba67ff7c822d0282a701de8c221fa1a32
taka <gdc01017@nifty.com>
2022-09-20 00:58:18 +0900

    fix branch original

--- calculator.py
+++ calculator.py
@@ -1,61 +1,28 @@
 import ipaddress
 
-def parse_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--network', help="The network in the format x.x.x.x/x.")
-    args = parser.parse_args()
-    return args.network
-
-
 class IPCalculator:
 
     def __init__(self, network):
         self.network = ipaddress.IPv4Network(network)
 
     def get_network(self):
-        """ Returns the network. """
-
         return str(self.network)
 
     def get_first_ip(self):
-        """ Calculates the first IP address. """
-
-        first_ip = list(self.network.hosts())[0]
+        nework_address = self.get_network_address()
+        first_ip = network_address + 1
         return str(first_ip)
 
     def get_last_ip(self):
-        """ Calculates the last IP address."""
-
-        last_ip = list(self.network.hosts())[-1]
+        broadcast_address = self.get_broadcast_address()
+        last_ip = broadcast_address -1
         return str(last_ip)
 
     def get_broadcast_address(self):
-        """ Calculates the broadcast address. """
-
         return self.network.broadcast_address
 
     def get_network_address(self):
-        """ Calculates the last address. """
-
         return self.network.network_address
 
     def get_netmask(self):
-        """ Calculate the netmask. """
-
         return self.network.netmask
-
-    def calculate(self):
-        """ Prints the calculation results. """
-
-        print('The results of the calculation is:')
-        print(f'-> Input network:\t\t{self.get_network()}')
-        print(f'-> Network address:\t\t{self.get_network_address()}')
-        print(f'-> Netmask:\t\t\t{self.get_netmask()}')
-        print(f'-> Broadcast address:\t\t{self.get_broadcast_address()}')
-        print(f'-> First IP address:\t\t{self.get_first_ip()}')
-        print(f'-> Last IP address:\t\t{self.get_last_ip()}')
-
-if __name__ == "__main__":
-    net = parse_arguments()
-    calc = IPCalculator(net)
-    calc.calculate()
