73eb8dc44f06052dd04effabcefa546cf970fbd5
taka <gdc01017@nifty.com>
2022-09-21 16:32:25 +0900

    Adding hotfix

--- calculator.py
+++ calculator.py
@@ -12,36 +12,42 @@ class IPCalculator:
     def __init__(self, network):
         self.network = ipaddress.IPv4Network(network)
 
+    @property
+    def broadcast_address(self):
+        if self.network: 
+            return self.network.broadcast_address
+
+    @property
+    def network_address(self):
+        if self.network:
+            return self.network.network_address
+
+    @property
+    def netmask(self):
+        if self.network:
+            return self.network.netmask
+
     def get_network(self):
         return str(self.network)
 
     def get_first_ip(self):
-        network_address = self.get_network_address()
-        first_ip = network_address + 1
+#        network_address = self.get_network_address()
+        first_ip = self.network_address + 1
         return str(first_ip)
 
     def get_last_ip(self):
-        broadcast_address = self.get_broadcast_address()
-        last_ip = broadcast_address -1
+#        broadcast_address = self.get_broadcast_address()
+        last_ip = self.broadcast_address -1
         return str(last_ip)
-
-    def get_broadcast_address(self):
-        return self.network.broadcast_address
-
-    def get_network_address(self):
-        return self.network.network_address
-
-    def get_netmask(self):
-        return self.network.netmask
-
+ 
     def calculate(self):
         """ Prints the calculation results. """
 
         print('The results of the calculation is:')
         print(f'-> Input network:\t\t{self.get_network()}')
-        print(f'-> Network address:\t\t{self.get_network_address()}')
-        print(f'-> Netmask:\t\t\t{self.get_netmask()}')
-        print(f'-> Broadcast address:\t\t{self.get_broadcast_address()}')
+#        print(f'-> Network address:\t\t{self.get_network_address()}')
+#        print(f'-> Netmask:\t\t\t{self.get_netmask()}')
+#        print(f'-> Broadcast address:\t\t{self.get_broadcast_address()}')
         print(f'-> First IP address:\t\t{self.get_first_ip()}')
         print(f'-> Last IP address:\t\t{self.get_last_ip()}')
 
